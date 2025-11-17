b7860a0f67868441ef2305007858e7857cba427b
DanielBeast11 <daniel-ahmerov@mail.ru>
2025-11-17 22:56:26 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/frontend-application-development/sw.js', { scope: '/frontend-application-development/' })})}
